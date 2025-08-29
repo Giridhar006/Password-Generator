824d381e66cf0619e45b9a8365c5625d80583c5d
Giridhar006 <annangigiridhar159@gmail.com>
2025-08-29 13:33:20 +0530

    modified program to accept usernames

--- Micro Project Code.py
+++ Micro Project Code.py
@@ -1,72 +1,77 @@
 #Password Generator Project
 
 import random
+import pickle
 
-def low_level_password(nrU_letters,nrL_letters,nr_symbols,nr_numbers,cdw):
+filename = "passwords.pkl"
+
+def ask_user(opt, nrU_letters, nrL_letters, nr_symbols, nr_numbers, cdw):
+    # Asks the user whether they like the generated password or not.
+    print("To select Yes, please enter 1.")
+    print("To select No, please enter 2.")
+    ask = input()
+    if ask == '1':
+        print("Thankyou")
+    elif ask == '2':
+        if opt == "low":
+            low_level_password(nrU_letters, nrL_letters, nr_symbols, nr_numbers, cdw)
+        elif opt == "high":
+            high_level_password(nrU_letters,nrL_letters,nr_symbols,nr_numbers,cdw)
+    else:
+        print("Invalid option")
+        ask_user()
+
+def low_level_password(nrU_letters, nrL_letters, nr_symbols, nr_numbers, cdw):
     #Eazy Level - Order not randomised:
     #e.g. 4 letter, 2 symbol, 2 number = JduE&!91
 
-    def ask_user():
-        # Asks the user whether they like the generated password or not.
-        print("To select Yes, please enter 1.")
-        print("To select No, please enter 2.")
-        ask = input()
-        if ask == '1':
-            print("Thankyou")
-        elif ask == '2':
-            low_level_password(nrU_letters,nrL_letters,nr_symbols,nr_numbers,cdw)
-        else:
-            print("Invalid option")
-            ask_user()
-            
     global password
     password = ""
-    for i in range(0, nrU_letters):
+
+    nrU_l = nrU_letters
+    while nrU_l > 0:
         # ASCII values of capital letters (65 to 90)
+        i = chr(random.randint(65,90))
         if i not in cdw:
-            password += chr(random.randint(65,90))
-    for i in range(0, nrL_letters):
+            password += i
+        nrU_l -= 1
+
+    nrL_l = nrL_letters
+    while nrL_l > 0:
         # ASCII values of small letters (97 to 122)
+        i = chr(random.randint(97,122))
         if i not in cdw:
-            password += chr(random.randint(97,122))
+            password += i
+        nrL_l -= 1
+
     nr_s = nr_symbols
     while nr_s > 0:
         # ASCII values of special characters (33 to 47), (58 to 64), (91 to 96), (123 to 126)
-        k = random.randint(33, 126)
-        if i not in cdw:
-            if not chr(k).isalnum():
-                password += chr(k)
-                nr_s -= 1
-    for i in range(0, nr_numbers):
+        i = chr(random.randint(33, 126))
+        if i not in cdw and not i.isalnum():
+            password += i
+            nr_s -= 1
+
+    nr_n = nr_numbers
+    while nr_n > 0:
         # ASCII values of numbers (48 to 57)
+        i = chr(random.randint(48,57))
         if i not in cdw:
-            password += chr(random.randint(48,57))
+            password += i
+        nr_n -= 1
+
     print(f"Here is your low level password: {password}")
     print("Do you want to set this as your password?\n")
-    ask_user()
-    
+    ask_user("low", nrU_letters, nrL_letters, nr_symbols, nr_numbers, cdw)
+
 def high_level_password(nrU_letters,nrL_letters,nr_symbols,nr_numbers,cdw):
     #High Level - Order of characters randomised:
     #e.g. 4 letter, 2 symbol, 2 number = g^2jk8&P
-    
-    def ask_user():
-        # Asks the user whether they like the generated password or not.
-        print("To select Yes, please enter 1.")
-        print("To select No, please enter 2.")
-        ask = input()
-        if ask == '1':
-            print("Thankyou")
-        elif ask == '2':
-            high_level_password(nrU_letters,nrL_letters,nr_symbols,nr_numbers,cdw)
-        else:
-            print("Invalid option")
-            ask_user()
 
     l_U = nrU_letters
     l_L = nrL_letters
     s = nr_symbols
     n = nr_numbers
-
     t = l_U + l_L + s + n
 
     global password
@@ -80,68 +85,98 @@ def high_level_password(nrU_letters,nrL_letters,nr_symbols,nr_numbers,cdw):
             if p not in cdw:
                 password += chr(p)
                 l_U -= 1
-            
+
         elif l_L != 0 and a == 1:
             # ASCII values of small letters (97 to 122)
             p = random.randint(97,122)
             if p not in cdw:
                 password += chr(p)
                 l_L -= 1
-            
+
         elif s != 0 and a == 2:
             # ASCII values of special characters (33 to 47), (58 to 64), (91 to 96), (123 to 126)
             i = random.randint(33, 126)
-            if i not in cdw:
-                if not chr(i).isalnum():
-                    password += chr(i)
-                    s -= 1
-            
+            if i not in cdw and not chr(i).isalnum():
+                password += chr(i)
+                s -= 1
+
         elif n != 0 and a == 3:
             # ASCII values of numbers (48 to 57)
             p = random.randint(48,57)
             if p not in cdw:
                 password += chr(p)
                 n -= 1
-            
+
         if len(password) == t:
             break
     print(f"Here is your high level password: {password}")
     print("Do you want to set this as your password?\n")
-    ask_user()
+    ask_user("high", nrU_letters, nrL_letters, nr_symbols, nr_numbers, cdw)
 
-def password_update(website):
-    # Updates the existing password
-    a = password + "\n"
-    filename = website
-    with open(filename, "a") as f:
-        f.write(a)
-        
+def password_update(username, website):
+
+    try:
+        # Updates the existing password
+        with open(filename, "rb") as file:
+            data = pickle.load(file)
+
+        if website in data:
+            if username in data[website]:
+                data[website][username].append(password)          # add password at the end
+            else:
+                data[website][username] = [password]
+        else:
+            data[website] = {}
+            data[website][username] = [password]
+
+        with open(filename, "wb") as file:
+            pickle.dump(data, file)
+
+    except:
+        # Creates new file and adds new password
+        data = {}
+        with open(filename, "wb") as file:
+            data[website] = {}
+            data[website][username] = [password]
+            pickle.dump(data, file)
+
 def view_password():
     # displays the present passwords for a website
-    try:
-        web = input("Which website do you need your password for?\n")
-        website = web.lower()
-        website += ".txt"
-        with open(website, 'r') as file:
-            password = file.readlines()[-1]
+
+    website = input("Which website do you need your password for?\n")
+    website = website.lower()
+
+    with open(filename, 'rb') as file:
+        data = pickle.load(file)
+    if website in data:
+        username = input("For which username do you need your password for?\n")
+        if username in data[website]:
+            password = data[website][username][-1]
             print(password)
-    except FileNotFoundError:
-        print(f"No password has been generated for {web}. Please generate a new password.\n")
-        view_password()
-        
+            file.close()
+        else:
+            print(f"No password has been generated for the website \"{website}\" with the username \"{username}\". Please generate a new password.\n")
+    else:
+        print(f"No password has been generated for the website \"{website}\". Please generate a new password.\n")
+
 def view_password_history():
     # displays all passwords for a website
-    try:
-        web = input("Which website's password history would you like to see?\n")
-        website = web.lower()
-        website += ".txt"
-        with open(website, 'r') as file:
-            passwords = file.readlines()
+    website = input("Which website's password history would you like to see?\n")
+    website = website.lower()
+
+    with open(filename, 'rb') as file:
+        data = pickle.load(file)
+    if website in data:
+        username = input("For which username do you need your password for?\n")
+        if username in data[website]:
+            passwords = data[website][username]
             for i in passwords:
                 print(i)
-    except FileNotFoundError:
-        print(f"No password has been generated for {web}. Please generate a new password.\n")
-        view_password_history()
+            file.close()
+        else:
+            print(f"No password has been generated for the website \"{website}\" with the username \"{username}\". Please generate a new password.\n")
+    else:
+        print(f"No password has been generated for the website \"{website}\". Please generate a new password.\n")
 
 def U_L():
     while True:
@@ -150,6 +185,7 @@ def U_L():
             return nrU_letters
         except:
             print("The option you selected is not valid. Please choose again.\n")
+
 def L_L():
     while True:
         try:
@@ -177,109 +213,121 @@ def N():
 def C_D_W():
     return list(map(ord,input("Please enter the characters you don't want in your password.\n")))
 
-def check_Key(file_path):
+def check_Key():
     try:
-
         # Open the file in read mode
-        with open(file_path, 'r') as file:
-
+        with open(filename, 'rb') as file:
             # Read the content of the file
-            file_content = file.read()
+            data = pickle.load(file)
 
-            if not file_content:
-                with open(file_path, 'w') as new_file:
-                    
-                    content = input("Please set a password for the password generator.\n")
-                    new_file.write(content)
-                    print("Great! Your password has been set.")
-                    return 1
+        if "default" not in data:
+            file.close()
+
+            p_1 = input("Please set a password for the password generator.\n")
+            p_2 = input("Confirm password.\n")
+            if p_1 == p_2:
+                data = {}
+                data["default"] = p_1
+                with open(filename, 'wb') as file:
+                    pickle.dump(data, file)
+                file.close()
+                print("Great! Your password has been set.")
+                check_Key()
             else:
+                print("passwords do not match, please try again.")
+                check_Key()
+        else:
+            # Prompt the user for input
+            p = input("Please enter the password for the password generator.\n")
 
-                # Prompt the user for input
-                user_input = input("Please enter the password for the password generator.\n")
+            # Compare user input with file content
+            if p == data["default"]:
+                print("Great! The password has been entered correctly.")
+                file.close()
+            else:
+                print("I apologize for the inconvenience. Please try entering the password again.")
+                file.close()
+                check_Key()
 
-                # Compare user input with file content
-                if user_input == file_content:
-                    print("Great! The password has been entered correctly.")
-                    return 1
-                else:
-                    print("I apologize for the inconvenience. Please try entering the password again.")
-                    check_Key(file_path)
-                    return 1   
     except FileNotFoundError:
-
         # Create a new file
-        with open(file_path, 'w') as new_file:
-            content = input("Please set a password for the password generator.\n")
-            new_file.write(content)
-            print("Great! Your password has been set.")
-            return 1
+        data = {}
+        with open(filename, "wb") as file:
+            pickle.dump(data, file)
+        check_Key()
 
-
-def update_Key(file_path):
-
+def update_Key():
     # Prompt the user for new content
-    new_content = input("To change the password for the password generator, please enter the new desired password.\n")
+    p_1 = input("To change the password for the password generator, please enter the new desired password.\n")
+    p_2 = input("Confirm password.\n")
 
-    # Open the file in write mode and overwrite the content
-    with open(file_path, 'w') as file:
-        file.write(new_content)
+    if p_1 == p_2:
+        # Open the file in write mode and overwrite the content
+        with open(filename, "rb") as file:
+            data = pickle.load(file)
+        file.close()
+
+        data["default"] = p_1
+        with open(filename, "wb") as file:
+            pickle.dump(data, file)
+        file.close()
+    else:
+        print("Passwords do ont match, please try again.")
+        update_Key()
 
     print("Excellent! The password for the password generator has been successfully updated.")
     print("Thankyou")
 
 def option():
-    if k == 1:
-        print()
-        print("To generate a low-level password, please enter 1.")
-        print("To generate a high-level password, please enter 2.")
-        print("To view your passwords, please enter 3.")
-        print("To view the password history for a particular website, please enter 4.")
-        print("To update the password for the password generator, please enter the number 5.")
-        print()
+    print()
+    print("To generate a low-level password, please enter 1.")
+    print("To generate a high-level password, please enter 2.")
+    print("To view your passwords, please enter 3.")
+    print("To view the password history for a particular website, please enter 4.")
+    print("To update the password for the password generator, please enter the number 5.")
+    print()
 
-        opt = input()
+    opt = input()
 
-        if opt == '1':
-            web = input("Which website would you like to generate a password for?\n")
-            website = web.lower()
-            website += ".txt"
-            nrU_letters = U_L()
-            nrL_letters = L_L()
-            nr_symbols = S()
-            nr_numbers = N()
-            cdw = C_D_W()
-            low_level_password(nrU_letters,nrL_letters,nr_symbols,nr_numbers,cdw)
-            password_update(website)
+    if opt == '1':
+        website = input("Which website would you like to generate a password for?\n")
+        website = website.lower()
+        username = input("For which username would you like to generate a password for?\n")
+        nrU_letters = U_L()
+        nrL_letters = L_L()
+        nr_symbols = S()
+        nr_numbers = N()
+        cdw = C_D_W()
+        low_level_password(nrU_letters, nrL_letters, nr_symbols, nr_numbers, cdw)
+        password_update(username, website)
 
-        elif opt == '2':
-    
-            web = input("Which website would you like to generate a password for?\n")
-            website = web.lower()
-            website += ".txt"
-            nrU_letters = U_L()
-            nrL_letters = L_L()
-            nr_symbols = S()
-            nr_numbers = N()
-            cdw = C_D_W()     
-            high_level_password(nrU_letters,nrL_letters,nr_symbols,nr_numbers,cdw)
-            password_update(website)
+    elif opt == '2':
 
-        elif opt == '3':
-            view_password()
-            
-        elif opt == '4':
-            view_password_history()
-            
-        elif opt == '5':
-            update_Key(file_path)
+        website = input("Which website would you like to generate a password for?\n")
+        website = website.lower()
+        username = input("For which username would you like to generate a password for?\n")
+        nrU_letters = U_L()
+        nrL_letters = L_L()
+        nr_symbols = S()
+        nr_numbers = N()
+        cdw = C_D_W()     
+        high_level_password(nrU_letters, nrL_letters, nr_symbols, nr_numbers, cdw)
+        password_update(username, website)
 
-        else:
-            print("The option you selected is not valid. Please choose again.\n")
-            option()
+    elif opt == '3':
+        view_password()
+
+    elif opt == '4':
+        view_password_history()
+
+    elif opt == '5':
+        update_Key()
+
+    else:
+        print("The option you selected is not valid. Please choose again.\n")
+        option()
 
 print("Welcome to the Password Generator!\n")
 
-file_path = 'key.txt'
-k = check_Key(file_path)
+check_Key()
 option()
